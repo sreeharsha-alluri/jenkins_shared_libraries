2c098be2ba50080a074ecbe59bad6c78c9eefaba
A S S K Sreeharsha <53439217+sreeharsha-alluri@users.noreply.github.com>
2024-05-27 11:45:45 +0530

    Create sendTeamsNotification.groovy

---
+++ teams/vars/sendTeamsNotification.groovy
@@ -0,0 +1,56 @@
+import groovy.json.JsonOutput
+ 
+void call(String webhookUrl, String status, String pipelineName, int buildNumber, String buildUrl) {
+    def themeColor
+    def activityTitle
+ 
+    switch(status) {
+        case "SUCCESS":
+            themeColor = '007300'
+            activityTitle = "Pipeline ${status}!"
+            break
+        case "FAILURE":
+            themeColor = 'FF0000'
+            activityTitle = "Pipeline ${status}!"
+            break
+        case "ABORTED":
+            themeColor = '808080'
+            activityTitle = "Pipeline ${status}!"
+            break
+        case "UNSTABLE":
+            themeColor = 'FFA500'
+            activityTitle = "Pipeline ${status}!"
+            break
+        default:
+            themeColor = '000000'
+            activityTitle = "Unknown Pipeline Status"
+            break
+    }
+ 
+    def payload = [
+        "@type": "MessageCard",
+        "@context": "http://schema.org/extensions",
+        "summary": "Pipeline ${status}",
+        "themeColor": themeColor,
+        "sections": [[
+            "activityTitle": activityTitle,
+            "facts": [
+                ["name": "Status", "value": status],
+                ["name": "Pipeline", "value": "<a href=\"$buildUrl\">${pipelineName} #${buildNumber}</a>"]
+            ]
+        ]]
+    ]
+ 
+    def jsonPayload = JsonOutput.toJson(payload)
+ 
+    try {
+        httpRequest(
+            contentType: 'APPLICATION_JSON',
+            httpMode: 'POST',
+            requestBody: jsonPayload,
+            url: webhookUrl
+        )
+    } catch (Exception e) {
+        echo "Failed to send notification: ${e.message}"
+    }
+}
