9f2c07aa56dd1920b751e410f614dfd4a6c1adbd
A S S K Sreeharsha <53439217+sreeharsha-alluri@users.noreply.github.com>
2024-06-11 14:40:29 +0530

    Update teamsWebhookUrl.groovy

--- teams/vars/teamsWebhookUrl.groovy
+++ teams/vars/teamsWebhookUrl.groovy
@@ -1,3 +1,3 @@
 def call() {
-    return 'https://amdcloud.webhook.office.com/webhookb2/133d4f16-689b-4281-a94b-44725b948c77@3dd8961f-e488-4e60-8e11-a82d994e183d/IncomingWebhook/8da07394137d4b8eb2c9bef3a79bc4b6/f4453ec5-087a-4294-9c4e-e643270504ff'
+    'https://amdcloud.webhook.office.com/webhookb2/133d4f16-689b-4281-a94b-44725b948c77@3dd8961f-e488-4e60-8e11-a82d994e183d/IncomingWebhook/50fa0ec4c1cf445e8abf60b42c917168/f4453ec5-087a-4294-9c4e-e643270504ff'
 }
