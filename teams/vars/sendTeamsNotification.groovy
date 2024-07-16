c9b53fa47300be349a49db1fe3f441799245d2b7
A S S K Sreeharsha <53439217+sreeharsha-alluri@users.noreply.github.com>
2024-07-16 17:03:46 +0530

    Update sendTeamsNotification.groovy

--- teams/vars/sendTeamsNotification.groovy
+++ teams/vars/sendTeamsNotification.groovy
@@ -1,54 +1,7 @@
 import groovy.json.JsonOutput
 
-void call(
-  String status,
-  String pipelineName,
-  int buildNumber,
-  String buildUrl,
-  String customMessage = '',
-  boolean onlyCustomMessage = false,
-  String mergedPRsMessageTeams = ''
-  ) {
+void call() {
     String webhookUrl = teamsWebhookUrl()
-    String themeColor
-    String activityTitle
-
-    switch (status) {
-        case 'SUCCESS':
-            themeColor = '007300'
-            activityTitle = "Pipeline SUCCESS!"
-            break
-        case 'FAILURE':
-            themeColor = 'FF0000'
-            activityTitle = "Pipeline FAILURE!"
-            break
-        case 'ABORTED':
-            themeColor = '808080'
-            activityTitle = "Pipeline ABORTED!"
-            break
-        case 'UNSTABLE':
-            themeColor = 'FFA500'
-            activityTitle = "Pipeline UNSTABLE!"
-            break
-        default:
-            themeColor = '000000'
-            activityTitle = "Unknown Pipeline Status"
-            break
-    }
-
-    List<Map<String, String>> facts = []
-
-    if (!onlyCustomMessage) {
-        facts.add(['title': 'Pipeline', 'value': "${pipelineName} #${buildNumber}"])
-    }
-
-    if (customMessage && !onlyCustomMessage) {
-        facts.add(['title': 'Message', 'value': customMessage])
-    }
-
-    if (mergedPRsMessageTeams) {
-        facts.add(['title': 'Merged PRs', 'value': mergedPRsMessageTeams])
-    }
 
     Map<String, Object> payload = [
         'type'    : 'AdaptiveCard',
@@ -56,14 +9,9 @@ void call(
         'body'    : [
             [
                 'type'   : 'TextBlock',
-                'text'   : activityTitle,
+                'text'   : 'Test Notification',
                 'weight' : 'Bolder',
-                'size'   : 'Medium',
-                'color'  : themeColor == 'FF0000' ? 'Attention' : themeColor == 'FFA500' ? 'Warning' : themeColor == '007300' ? 'Good' : 'Default'
-            ],
-            [
-                'type' : 'FactSet',
-                'facts': facts
+                'size'   : 'Medium'
             ]
         ]
     ]
