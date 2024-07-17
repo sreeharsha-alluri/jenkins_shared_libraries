5acf4573fc132b5a46caa8290e6907d6902fb946
A S S K Sreeharsha <53439217+sreeharsha-alluri@users.noreply.github.com>
2024-07-17 10:23:02 +0530

    Update sendTeamsNotification.groovy

--- teams/vars/sendTeamsNotification.groovy
+++ teams/vars/sendTeamsNotification.groovy
@@ -1,25 +1,81 @@
 import groovy.json.JsonOutput
 
-void call() {
+void call(
+  String status,
+  String pipelineName,
+  int buildNumber,
+  String buildUrl,
+  String customMessage = '',
+  boolean onlyCustomMessage = false,
+  String mergedPRsMessageTeams = ''
+) {
     String webhookUrl = teamsWebhookUrl()
+    String themeColor
+    String activityTitle
+    String icon = teamsIcon(status)
+
+    if (onlyCustomMessage) {
+        themeColor = '008080'
+        activityTitle = customMessage
+    } else {
+        switch (status) {
+            case 'SUCCESS':
+                themeColor = '007300'
+                activityTitle = "${icon} Pipeline ${status}!"
+                break
+            case 'FAILURE':
+                themeColor = 'FF0000'
+                activityTitle = "${icon} Pipeline ${status}!"
+                break
+            case 'ABORTED':
+                themeColor = '808080'
+                activityTitle = "${icon} Pipeline ${status}!"
+                break
+            case 'UNSTABLE':
+                themeColor = 'FFA500'
+                activityTitle = "${icon} Pipeline ${status}!"
+                break
+            default:
+                themeColor = '000000'
+                activityTitle = "${icon} Unknown Pipeline Status"
+                break
+        }
+    }
+
+    List<Map<String, Object>> facts = []
+
+    if (!onlyCustomMessage) {
+        facts.add(['title': 'Pipeline', 'value': "<a href=\"$buildUrl\">${pipelineName} #${buildNumber}</a>"])
+    }
+
+    if (customMessage && !onlyCustomMessage) {
+        facts.add(['title': '', 'value': teamsBold(customMessage)])
+    }
+
+    if (mergedPRsMessageTeams) {
+        facts.add(['title': '', 'value': mergedPRsMessageTeams])
+    }
 
     Map<String, Object> payload = [
-        'type'    : 'AdaptiveCard',
-        'version' : '1.2',
-        'body'    : [
-            [
+        'type'       : 'AdaptiveCard',
+        'context'    : 'https://schema.org/extensions',
+        'themeColor' : themeColor,
+        'version'    : '1.0',
+        'body'       : [[
+            'type': 'Container',
+            'items': [[
                 'type'   : 'TextBlock',
-                'text'   : 'Test Notification',
-                'weight' : 'Bolder',
-                'size'   : 'Medium'
-            ]
-        ]
+                'text'   : activityTitle,
+                'weight' : 'bolder',
+                'size'   : 'medium'
+            ], [
+                'type': 'FactSet',
+                'facts': facts
+            ]]
+        ]]
     ]
 
     String jsonPayload = JsonOutput.toJson(payload)
-
-    // Log the payload for debugging
-    echo "Payload: ${jsonPayload}"
 
     try {
         httpRequest(
