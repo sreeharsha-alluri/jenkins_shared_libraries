7a4d9c60352ed662c088765bb3cc905701036e49
A S S K Sreeharsha <53439217+sreeharsha-alluri@users.noreply.github.com>
2024-07-24 14:36:51 +0530

    Update sendTeamsNotification.groovy

--- teams/vars/sendTeamsNotification.groovy
+++ teams/vars/sendTeamsNotification.groovy
@@ -21,6 +21,7 @@ void call(String status, String jobName, int buildNumber, String buildUrl, Strin
     }
 
     if (customMessage) {
+        customMessage = customMessage.replaceAll('\n', '\\n')
         // Use \n for line breaks in the message
         bodyElements += [
             [
